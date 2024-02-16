3753cea9ce121f8a87d374c34f3c2fc5c55d8d6b
jbellister-slac <jesseb@slac.stanford.edu>
2024-02-16 13:35:44 -0800

    MAINT: Fix convert command. Hard code to LCLS for now. Make it one yaml file for now

--- xml/lint_xml.sh
+++ xml/lint_xml.sh
@@ -3,8 +3,11 @@
 
 exit_code=0
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-for FILE in $SCRIPT_DIR/*.xml; do
-  xmllint --noout --schema $SCRIPT_DIR/phoebus-alarm-server.xsd $FILE || ((exit_code++));
-done
+#for FILE in $SCRIPT_DIR/*.xml; do
+#  xmllint --noout --schema $SCRIPT_DIR/phoebus-alarm-server.xsd $FILE || ((exit_code++));
+#done
 
+# Temporarily run on lcls only, restore once alarm server restart is more stable
+FILE="$SCRIPT_DIR/lcls.xml"
+xmllint --noout --schema "$SCRIPT_DIR/server.xsd" "$FILE" || ((exit_code++))
 exit $exit_code
