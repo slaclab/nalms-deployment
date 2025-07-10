62f0d6c22592e1d8e9591f38fe2cdd7bfdbf6287
jbellister-slac <89539359+jbellister-slac@users.noreply.github.com>
2025-07-10 14:59:07 -0700

    Update lint_xml.sh to run on eventual facet config

--- xml/lint_xml.sh
+++ xml/lint_xml.sh
@@ -7,7 +7,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 #  xmllint --noout --schema $SCRIPT_DIR/phoebus-alarm-server.xsd $FILE || ((exit_code++));
 #done
 
-# Temporarily run on lcls only, restore once alarm server restart is more stable
-FILE="$SCRIPT_DIR/lcls.xml"
+# Temporarily run on facet only, restore once LCLS config is stable
+FILE="$SCRIPT_DIR/facet.xml"
 xmllint --noout --schema "$SCRIPT_DIR/phoebus-alarm-server.xsd" "$FILE" || ((exit_code++))
 exit $exit_code
